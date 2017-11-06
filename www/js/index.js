e423e1641acd06d8ce6d37fb1e519e87a8bfc688
DeceptiveHedge <monj1_14@uni.worc.ac.uk>
2017-11-06 14:33:08 +0000

    Updated app to perform touch input

--- www/js/index.js
+++ www/js/index.js
@@ -1,4 +1,28 @@
+// For more info on jQuery Mobile,  touch gestures and other useful events see : http://api.jquerymobile.com/category/events/ -->
+
 $(document).on("pagecreate","#pageone",function(){
+  
+  	$('#tapholdtext').on("taphold",function(){
+    	$(this).hide();
+ 	});                       
+
+	$('#taptext').on("tap",function(){
+    	alert("Stop tapping!");
+ 	}); 
+
+	$('#swipetext').on("swipeleft",function(){
+    	$(this).css('color', 'green');
+  	});   
+
+
+});
+
+
+
+
+
+
+/*$(document).on("pagecreate","#pageone",function(){
   $('#submitButton').on("click", function(){
     submitText();
   });            
@@ -14,6 +38,6 @@ function submitText() {
 function storeValue(key, value) {
 	//add some code to store the key-value pair in persistant storage 
     window.localStorage.setItem(key, value);
-}
+}*/
 
 
